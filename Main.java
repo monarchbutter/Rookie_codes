4ab4dfba514ef1cf51b2b62f99bc87fc02becd02
monarchbutter <147073637+monarchbutter@users.noreply.github.com>
2025-07-20 00:14:10 +0530

    Insane Dice Rolling

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+     public static void main(String[] args) {
+
+         Scanner scanner = new Scanner(System.in);
+         Random random = new Random();
+
+         int number = random.nextInt(1,1000000);
+         int roll = 1;
+
+         while(number != 696969) {
+             System.out.println(number);
+             number = random.nextInt(1,1000000);
+             System.out.println(number);
+             roll++;
+
+         }
+
+         System.out.println("congrats we finally won");
+         System.out.println("it took  us " + roll + " rolls");
+
+         scanner.close();
+    }
+
+}
